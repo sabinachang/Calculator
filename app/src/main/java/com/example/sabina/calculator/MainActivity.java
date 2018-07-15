4dda756ecd8cd791235dabbc380bf4b188933517
sabinachang <sabchang1@gmail.com>
2018-07-15 12:03:13 +0800

    Initial commit. Create calculator layout.

---
+++ app/src/main/java/com/example/sabina/calculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sabina.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private ImageButton
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
